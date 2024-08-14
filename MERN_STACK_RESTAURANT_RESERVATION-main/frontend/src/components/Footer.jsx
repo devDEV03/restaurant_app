b8b13c8e5972fe32e38ae7b4c91fde8c02da7da7
gourvi <gourvi@gmail.com>
2024-08-15 03:02:46 +0530

    Initial commit

---
+++ MERN_STACK_RESTAURANT_RESERVATION-main/frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Gourvi</div>
+          <div className="right">
+            <p>Z 9, Civil Lines, Bareilly,U.P.</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By GOURVI</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By GOURVI.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
