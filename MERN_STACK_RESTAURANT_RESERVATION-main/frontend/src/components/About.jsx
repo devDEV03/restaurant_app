4369d09eb31a6436c133bf51e48bfd6f3afc7746
Gourvi Siriah <151432668+gsiriah@users.noreply.github.com>
2024-08-17 13:40:32 +0530

    Update About.jsx
    changes

--- MERN_STACK_RESTAURANT_RESERVATION-main/frontend/src/components/About.jsx
+++ MERN_STACK_RESTAURANT_RESERVATION-main/frontend/src/components/About.jsx
@@ -13,13 +13,12 @@ const About = () => {
               <p>The only thing we're serious about is food.</p>
             </div>
             <p className="mid">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Provident possimus optio adipisci dolores impedit illum iusto
-              perferendis, laudantium quod accusamus consequuntur consectetur,
-              tempore nulla error iure reiciendis dolorem assumenda.
-              Necessitatibus fugit asperiores totam rem esse exercitationem
-              iusto ipsum qui dolore ex, accusantium repellat mollitia
-              repellendus.
+            "Welcome to GOFOOD
+At GOFOOD, we are passionate about bringing the freshest and most delicious [type of cuisine] to your table. Our chefs craft each dish with love and attention, using only the finest ingredients sourced locally whenever possible. We believe that great food comes from the heart, and we are committed to creating a dining experience that you'll cherish.
+
+Founded in [Year], [Restaurant Name] started as a dream to share our love for [type of cuisine] with the community. Over the years, we've grown, but our dedication to quality and customer satisfaction remains unchanged. Whether you're enjoying a meal with family or grabbing a quick bite, we aim to make every visit special.
+
+Our team, led by [Head Chef/Owner Name], is a group of passionate individuals who are dedicated to serving you the best. We believe in sustainability, and we work hard to reduce our environmental impact through eco-friendly practices..
             </p>
             <Link to={"/"}>
               Explore Menu{" "}
